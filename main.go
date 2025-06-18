f246aa76eed40573b7b129668ac2d7ef68691d85
Ji Choi <jchoi@aiconstack.com>
2025-06-18 07:51:35 -0400

    add weather struct

--- main.go
+++ main.go
@@ -12,6 +12,37 @@ import (
 )
 
 type Weather struct {
+	Location struct {
+		Name      string `json:"name"`
+		State     string `json:"state"`
+		Country   string `json:"country"`
+		LocalTime string `json:"localtime"`
+	} `json:"location"`
+	Current struct {
+		Temp       float64 `json:"temp_f"`
+		Humidity   int16   `json:"humidity"`
+		Cloud      int16   `json:"cloud"`
+		FeelsLike  float64 `json:"feelslike_f"`
+		UV         float64 `json:"uv"`
+		AirQuality struct {
+			PM25 float64 `json:"pm2_5"`
+		} `json:"air_quality"`
+		Condition struct {
+			Text string `json:"text"`
+		} `json:"condition"`
+	} `json:"current"`
+	Forecast struct {
+		Forecastday []struct {
+			Hour []struct {
+				TimeEpoch    int64   `json:"time_epoch"`
+				TempF        float64 `json:"temp_f"`
+				ChanceOfRain float64 `json:"chance_of_rain"`
+				Condition    struct {
+					Text string `json:"text"`
+				} `json:"condition"`
+			} `json:"hour"`
+		} `json:"forecastday"`
+	} `json:"forecast"`
 }
 
 func main() {
